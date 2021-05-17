67ccc7226b6fb241360ae57c5351b09f29bf9852
divided7 <41837037+divided7@users.noreply.github.com>
2021-05-18 04:35:21 +0530

    backend done

--- backend/blockchain/blockchain.py
+++ backend/blockchain/blockchain.py
@@ -1,5 +1,6 @@
 from backend.blockchain.block import Block
 from backend.wallet.transaction import Transaction
+from backend.wallet.wallet import Wallet
 
 from backend.config import MINING_REWARD_INPUT
 
@@ -62,10 +63,12 @@ class Blockchain:
     for i in range(1, len(chain)):
       block = chain[i]
       last_block = chain[i - 1]
-      Block.is_valid_block(last_block,block)
+      Block.is_valid_block(last_block, block)
+    
+    Blockchain.is_valid_transaction_chain(chain)
 
   @staticmethod
-  def is_valid_transaction(chain):
+  def is_valid_transaction_chain(chain):
     """
     enforce the rules of a chain composed of blocks of transactions
       - each transaction must only appear once in a chain
@@ -73,23 +76,30 @@ class Blockchain:
       - each transaction must be valid
     """
     transaction_ids=set()
-    for block in chain:
+    for i in range(len(chain)):
+      block = chain[i]
       has_mining_reward = False
       for transaction_json in block.data:
         transaction = Transaction.from_json(transaction_json)
-        
-        if transaction.input == MINING_REWARD_INPUT:
-          if has_mining_reward == True:
-            raise Exception('There can only be one mining reward per block!'+f' Check block with hash {block.hash}')
-          has_mining_reward = True
-          
-
         if transaction.id in transaction_ids:
           raise Exception(f'Transaction {transaction.id} is not unique!')
         
         transaction_ids.add(transaction.id)
 
-        Transaction.is_valid_transaction(transaction)
+        if transaction.input == MINING_REWARD_INPUT:
+          if has_mining_reward == True:
+            raise Exception('There can only be one mining reward per block!'+f' Check block with hash {block.hash}')
+          has_mining_reward = True
+        else:
+          blockchain_history = Blockchain()
+          blockchain_history.chain = chain[0:i]
+
+          balance_history = Wallet.calculate_balance(blockchain_history,transaction.input['address'])
+    
+          if balance_history != transaction.input['amount']:
+            raise Exception(f'Transaction {transaction.id} has an invalid input amount!')
+
+          Transaction.is_valid_transaction(transaction)
 
 def main():
   blockchain= Blockchain()
--- backend/tests/blockchain/test_blockchain.py
+++ backend/tests/blockchain/test_blockchain.py
@@ -1,5 +1,9 @@
 from backend.blockchain.blockchain import Blockchain
 from backend.blockchain.block import GENESIS_DATA
+
+from backend.wallet.transaction import Transaction
+from backend.wallet.wallet import Wallet
+
 import pytest
 
 def test_blockchain_instance():
@@ -17,7 +21,7 @@ def test_add_block():
 def populated_blockchain():
   blockchain = Blockchain()
   for i in range(3):
-    blockchain.add_block(i)
+    blockchain.add_block([Transaction(Wallet(),'recipient',i).to_json()])
   return blockchain
 
 def test_is_valid_chain(populated_blockchain):
@@ -47,3 +51,40 @@ def test_replace_chain_invalid(populated_blockchain):
 
   with pytest.raises(Exception,match='Cannot replace! Incoming chain is not valid!'):
     blockchain.replace_chain(populated_blockchain.chain)
+
+def test_valid_transaction_chain(populated_blockchain):
+  Blockchain.is_valid_transaction_chain(populated_blockchain.chain)
+
+def test_valid_transaction_chain_duplicate_transactions(populated_blockchain):
+  transaction = Transaction(Wallet(),'recipient',1).to_json()
+  populated_blockchain.add_block([transaction, transaction])
+  
+  with pytest.raises(Exception,match='is not unique!'):
+    Blockchain.is_valid_transaction_chain(populated_blockchain.chain)
+
+def test_valid_transaction_chain_multiple_rewards(populated_blockchain):
+  reward_1 = Transaction.reward_transaction(Wallet()).to_json()
+  reward_2 = Transaction.reward_transaction(Wallet()).to_json()
+
+  populated_blockchain.add_block([reward_1, reward_2])
+  
+  with pytest.raises(Exception,match='one mining reward per block'):
+    Blockchain.is_valid_transaction_chain(populated_blockchain.chain)
+
+def test_valid_transaction_chain_bad_transaction(populated_blockchain):
+  bad_transaction = Transaction(Wallet(), 'recipient', 1)
+  bad_transaction.input['signature'] = Wallet().sign(bad_transaction.output)
+  populated_blockchain.add_block([bad_transaction.to_json()])
+  with pytest.raises(Exception):
+    Blockchain.is_valid_transaction_chain(populated_blockchain.chain)
+  
+def test_is_valid_transaction_chain_bad_balance_history(populated_blockchain):
+  wallet = Wallet()
+  bad_transaction = Transaction(wallet, 'recipient', 1)
+  bad_transaction.output[wallet.address] = 9000
+  bad_transaction.input['amount'] = 9001
+  bad_transaction.input['signature'] = wallet.sign(bad_transaction.output)
+
+  populated_blockchain.add_block([bad_transaction.to_json()])
+  with pytest.raises(Exception,match='invalid input amount'):
+    Blockchain.is_valid_transaction_chain(populated_blockchain.chain)
